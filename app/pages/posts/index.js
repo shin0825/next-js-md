de812843e8c96edae4cc58ac53e1a9047129a480
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-07-09 12:52:45 +0900

    allPostsDataからdateを取得して表示した

--- app/pages/posts/index.js
+++ app/pages/posts/index.js
@@ -15,11 +15,12 @@ export default function Index({ allPostsData }) {
   return (
     <Layout home>
       <h2>POSTS</h2>
-        {allPostsData.map(({ id }) => (
+        {allPostsData.map(({ id, title, date }) => (
           <li key={id}>
             <Link href="/posts/[id]" as={`/posts/${id}`}>
-              <a>{id}</a>
+              <a>{title}</a>
             </Link>
+            <p>{date}</p>
           </li>
         ))}
     </Layout>
