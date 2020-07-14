97f254a60b0a52c278a7dfb22ce679ca8fa030fe
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-07-14 12:53:52 +0900

    デザイン調整

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,10 +1,11 @@
+import styles from './Layout.module.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Link from 'next/link'
 
 export default function Layout({ children, home }) {
   return (
-    <div>
+    <div className = { styles.container }>
       <Header />
       <main>{children}</main>
       {!home && (
--- app/components/Menu.js
+++ app/components/Menu.js
@@ -1,15 +1,26 @@
+import styles from './Menu.module.css'
 import Link from 'next/link';
 
 export default function Menu() {
   return (
-    <div>
-      <Link href="/"><a>Home</a></Link>
-      <ul>
-        <li>Blog</li>
-        <li><Link href="/posts"><a>Drawing</a></Link></li>
-        <li>Programming</li>
+    <nav className={styles.navigator}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <Link href="/">
+            <a className={styles.link}>Home</a>
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link href="/about">
+            <a className={styles.link}>About</a>
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link href="/posts">
+            <a className={styles.link}>Blog</a>
+          </Link>
+        </li>
       </ul>
-      <Link href="/about"><a>About</a></Link>
-    </div>
+    </nav>
   )
 }
