df30591f42190eee874cea1b19bc2dafe59c41ac
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-07-15 12:52:57 +0900

    共通のページ設定用にファイルを追加

---
+++ app/pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel='stylesheet'
+          />
+          <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
