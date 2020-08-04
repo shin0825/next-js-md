97f19fe6f3c0c3f83fef0f74e1e9d06b5a43d6ff
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-08-05 00:38:07 +0900

    aboutに自分の制作物を追加

--- app/pages/about.js
+++ app/pages/about.js
@@ -22,6 +22,101 @@ export default function About() {
               </p>
             </div>
           </div>
+          <h2 className={styles.title}>個人制作物</h2>
+          <div className={styles.main}>
+            <img src="/portfolio.png" alt="my photo" className={styles.siteimage} />
+            <div className={styles.text}>
+              <h2>ポートフォリオ</h2>
+              <a href="https://s-nakamura-portfolio.com/">https://s-nakamura-portfolio.com/</a>
+              <p>
+                このサイト。
+              </p>
+              <div className={styles.table}>
+                <h3>Web</h3>
+                <p>Next.js</p>
+              </div>
+              <div className={styles.table}>
+                <h3>Hosting</h3>
+                <p>Vercel</p>
+              </div>
+              <div className={styles.table}>
+                <h3>Domain</h3>
+                <p>Google Domains</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.main}>
+            <img src="/nameless.png" alt="my photo" className={styles.siteimage} />
+            <div className={styles.text}>
+              <h2>記事のURLを投げるとタイトルを取ってきて一緒に保存してくれるアプリ</h2>
+              <a href="https://nameless-tundra-72223.herokuapp.com/">https://nameless-tundra-72223.herokuapp.com/</a>
+              <p>
+                あとで読みたい記事のURLとタイトルだけを保存してくれるアプリ。<br />
+                溜まる一方。
+              </p>
+              <div className={styles.table}>
+                <h3>Web</h3>
+                <p>Vue.js, Ruby on Rails</p>
+              </div>
+              <div className={styles.table}>
+                <h3>DB</h3>
+                <p>PostgreSQL</p>
+              </div>
+              <div className={styles.table}>
+                <h3>Hosting</h3>
+                <p>Heroku</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.main}>
+            <img src="/otogemap.png" alt="my photo" className={styles.siteimage} />
+            <div className={styles.text}>
+              <h2>音ゲーマップ</h2>
+              <a href="https://otogemap.herokuapp.com/">https://otogemap.herokuapp.com/</a>
+              <p>
+                ゲーセンに設置されているIIDXの情報を<br />
+                データベース化したいと思い開発。<br />
+                仲間内で利用しようとしていたら<br />
+                世間がゲーセンにいけない感じになってしまいました。。。
+              </p>
+              <div className={styles.table}>
+                <h3>Web</h3>
+                <p>Ruby on Rails</p>
+              </div>
+              <div className={styles.table}>
+                <h3>DB</h3>
+                <p>PostgreSQL</p>
+              </div>
+              <div className={styles.table}>
+                <h3>Hosting</h3>
+                <p>Heroku</p>
+              </div>
+              <div className={styles.table}>
+                <h3>API</h3>
+                <p>GCP Geocoding API</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.main}>
+            <img src="/kakeibo.gif" alt="my photo" className={styles.siteimage} />
+            <div className={styles.text}>
+              <h2>自分用家計簿</h2>
+              <a href="#">https://ガチの自分用家計簿なので非公開/</a>
+              <p>
+                すぐ使ったお金を記録したいと思って簡素なWebアプリを作成。<br />
+                費目を探すのが結構面倒臭いので登録をLINE APIなどにするとか<br />
+                改善点はまだまだある模様　運用でカバーしている現状です
+              </p>
+              <div className={styles.table}>
+                <h3>Web</h3>
+                <p>Ruby on Rails</p>
+              </div>
+              <div className={styles.table}>
+                <h3>DB</h3>
+                <p>PostgreSQL</p>
+              </div>
+            </div>
+          </div>
         </div>
       </Layout>
     </div>
