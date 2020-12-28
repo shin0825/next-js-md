5dddd1f3f1d1bd37f3fbf4ebd19b0f8818c69b5b
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-12-28 23:04:48 +0900

    update pages skill.js コンテツを表示するコンテナをとりあえず準備

--- app/pages/skill.js
+++ app/pages/skill.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout'
 import Skill from '../components/Skill'
 import { getSortedSkillsData } from '../lib/skills'
 import Link from 'next/link'
-import styles from './index.module.css'
+import styles from './skill.module.css'
 
 export async function getStaticProps() {
   const allSkillsData = getSortedSkillsData()
@@ -17,11 +17,11 @@ export default function Index({ allSkillsData }) {
   return (
     <Layout>
       <h2 className={styles.title}>スキルセット</h2>
-        <div className={styles.flexbox}>
-          {allSkillsData.map(({ id, title, date }) => (
-            <Skill id={id} title={title} date={date} />
-          ))}
-        </div>
+      <div className={styles.flexbox}>
+        {allSkillsData.map(({ id, title, date }) => (
+          <Skill id={id} title={title} date={date} />
+        ))}
+      </div>
     </Layout>
   )
 }
