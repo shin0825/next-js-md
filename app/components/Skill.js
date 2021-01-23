b5ea8342d662b1d372c9e1d91854dce0210fd1cc
shin0825 <43142294+shin0825@users.noreply.github.com>
2021-01-23 23:58:18 +0900

    update Skill image text css

--- app/components/Skill.js
+++ app/components/Skill.js
@@ -4,7 +4,7 @@ export default function Skill({ id, image, title, date_from, date_to, context })
   return (
     <div className={styles.main}>
       {(image) && (<img src={image} className={styles.image} />)}
-      {(!image) && (<p className={styles.image}></p>)}
+      {(!image) && (<div className={styles.image}><p>{title}</p></div>)}
       <div className={styles.text}>
         <h2>{title}</h2>
         <p>{date_from} ~ {date_to}</p>
